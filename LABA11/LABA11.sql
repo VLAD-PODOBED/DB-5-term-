5f23df905f0b2a43e19ca09a6ed2a9c53e95cdb3
Vlad <mama2003271@gmail.com>
2023-12-24 12:20:53 +0300

    add

--- LABA11/LABA11.sql
+++ LABA11/LABA11.sql
@@ -1,199 +1,169 @@
 ------------------------------------ Задание №1
-create or replace procedure C##PVG_P.GET_TEACHERS(PCODE TEACHER.PULPIT%TYPE)
-    is
-    cursor GetTeachers is select TEACHER, TEACHER_NAME, PULPIT
-                          from TEACHER
-                          where PULPIT = PCODE;
-    m_teacher      TEACHER.TEACHER%TYPE;
-    m_teacher_name TEACHER.TEACHER_NAME%TYPE;
-    m_pulpit       TEACHER.PULPIT%TYPE;
-begin
-    open GetTeachers;
-    fetch GetTeachers into m_teacher, m_teacher_name, m_pulpit;
-    DBMS_OUTPUT.PUT_LINE(m_teacher || ' ' || m_teacher_name || ' ' || m_pulpit);
-
-    while (GetTeachers%found)
-        loop
-            DBMS_OUTPUT.PUT_LINE(m_teacher || ' ' || m_teacher_name || ' ' || m_pulpit);
-            fetch GetTeachers into m_teacher, m_teacher_name, m_pulpit;
-        end loop;
-    close GetTeachers;
-end GET_TEACHERS;
-
-begin
-    GET_TEACHERS('ИСиТ');
-end;
+DECLARE
+   PROCEDURE GET_TEACHERS(PCODE IN TEACHER.PULPIT%TYPE) IS 
+      CURSOR GetTeachers IS 
+               SELECT TEACHER, TEACHER_NAME, PULPIT
+               FROM TEACHER
+               WHERE PULPIT = PCODE;
+      m_teacher      TEACHER.TEACHER%TYPE;
+      m_teacher_name TEACHER.TEACHER_NAME%TYPE;
+      m_pulpit       TEACHER.PULPIT%TYPE;
+   BEGIN
+      FOR teacher_rec IN GetTeachers LOOP
+         DBMS_OUTPUT.PUT_LINE(teacher_rec.TEACHER || ' ' || teacher_rec.TEACHER_NAME || ' ' || teacher_rec.PULPIT);
+      END LOOP;
+   END GET_TEACHERS;
+BEGIN
+   GET_TEACHERS('ИСиТ');
+END;
 ------------------------------------ Задание №2
-create or replace function C##PVG_P.GET_NUM_TEACHERS (PCODE TEACHER.PULPIT%TYPE)
-return number
-is
-    result_num number;
-begin
-    select count(TEACHER) into result_num from TEACHER where PULPIT=PCODE;
-    return result_num;
-end GET_NUM_TEACHERS;
-
-begin
-     DBMS_OUTPUT.PUT_LINE(GET_NUM_TEACHERS('ИСиТ'));
-end;
+DECLARE
+   FUNCTION GET_NUM_TEACHERS(PCODE IN TEACHER.PULPIT%TYPE) RETURN NUMBER IS 
+      result_num NUMBER;
+   BEGIN
+      SELECT COUNT(TEACHER) INTO result_num FROM TEACHER WHERE PULPIT = PCODE;
+      RETURN result_num;
+   END GET_NUM_TEACHERS;
+BEGIN
+   DBMS_OUTPUT.PUT_LINE(GET_NUM_TEACHERS('ИСиТ'));
+END;
 ------------------------------------ Задание №3
-create or replace procedure C##PVG_P.GET_TEACHERS_BY_FACULTY (FCODE FACULTY.FACULTY%TYPE)
-    is
-    cursor GetTeachersByFaculty is
-        select TEACHER, TEACHER_NAME, P.PULPIT
-        from TEACHER inner join PULPIT P on P.PULPIT = TEACHER.PULPIT
-        where FACULTY = FCODE;
-
-    m_teacher      TEACHER.TEACHER%TYPE;
-    m_teacher_name TEACHER.TEACHER_NAME%TYPE;
-    m_pulpit       TEACHER.PULPIT%TYPE;
-begin
-    open GetTeachersByFaculty;
-    fetch GetTeachersByFaculty into m_teacher, m_teacher_name, m_pulpit;
-
-    while (GetTeachersByFaculty%found)
-    loop
-        DBMS_OUTPUT.PUT_LINE(m_teacher || ' ' || m_teacher_name || ' ' || m_pulpit);
-        fetch GetTeachersByFaculty into m_teacher, m_teacher_name, m_pulpit;
-    end loop;
-
-    close GetTeachersByFaculty;
-
-end GET_TEACHERS_BY_FACULTY;
-
-begin
-    GET_TEACHERS_BY_FACULTY('ИДиП');
-end;
+DECLARE
+    PROCEDURE GET_TEACHERS(PCODE IN TEACHER.PULPIT%TYPE) IS
+        CURSOR GetTeachers IS
+            SELECT TEACHER, TEACHER_NAME, PULPIT
+            FROM TEACHER
+            WHERE PULPIT = PCODE;
+    BEGIN
+        FOR teacher_rec IN GetTeachers LOOP
+            DBMS_OUTPUT.PUT_LINE(teacher_rec.TEACHER || ' ' || teacher_rec.TEACHER_NAME || ' ' || teacher_rec.PULPIT);
+        END LOOP;
+    END GET_TEACHERS;
+BEGIN
+    GET_TEACHERS('ИСиТ');
+END;
 
 
-create or replace procedure C##PVG_P.GET_SUBJECTS (PCODE SUBJECT.PULPIT%TYPE)
-is
-    cursor GetSubjects is
-    select * from SUBJECT where PULPIT=PCODE;
+DECLARE
+   PROCEDURE GET_SUBJECTS(PCODE IN SUBJECT.PULPIT%TYPE) IS 
+      CURSOR SubjectsCursor IS 
+               SELECT SUBJECT, SUBJECT_NAME, PULPIT
+               FROM SUBJECT
+               WHERE PULPIT = PCODE;
+      m_subject      SUBJECT.SUBJECT%TYPE;
+      m_subject_name SUBJECT.SUBJECT_NAME%TYPE;
+      m_pulpit       SUBJECT.PULPIT%TYPE;
+   BEGIN
+      FOR subject_rec IN SubjectsCursor LOOP
+         DBMS_OUTPUT.PUT_LINE(subject_rec.SUBJECT || ' ' || subject_rec.SUBJECT_NAME || ' ' || subject_rec.PULPIT);
+      END LOOP;
+   END GET_SUBJECTS;
+BEGIN
+   GET_SUBJECTS('ИСиТ');
+END;
 
-    m_subject SUBJECT.SUBJECT%type;
-    m_subject_name SUBJECT.SUBJECT_NAME%type;
-    m_pulpit SUBJECT.PULPIT%type;
-begin
-    open GetSubjects;
-    fetch GetSubjects into m_subject, m_subject_name, m_pulpit;
-
-    while (GetSubjects%found)
-    loop
-        DBMS_OUTPUT.PUT_LINE(m_subject || ' ' || m_subject_name || ' ' || m_pulpit);
-        fetch GetSubjects into m_subject, m_subject_name, m_pulpit;
-    end loop;
-    close GetSubjects;
-
-end GET_SUBJECTS;
-
-begin
-    GET_SUBJECTS('ИСиТ');
-end;
 
 ------------------------------------ Задание №4
-create or replace function C##PVG_P.FGET_NUM_TEACHERS (FCODE FACULTY.FACULTY%TYPE)
-return number
-is
-    result_num number;
-begin
-    select count(TEACHER) into result_num from TEACHER
-                          where TEACHER.PULPIT in (select PULPIT from PULPIT where FACULTY=FCODE);
-    return result_num;
-end FGET_NUM_TEACHERS;
+DECLARE
+   FUNCTION FGET_NUM_TEACHERS(FCODE IN FACULTY.FACULTY%TYPE) RETURN NUMBER IS 
+      result_num NUMBER;
+   BEGIN
+      SELECT COUNT(TEACHER) INTO result_num
+      FROM TEACHER
+      WHERE TEACHER.PULPIT IN (SELECT PULPIT FROM PULPIT WHERE FACULTY = FCODE);
+      RETURN result_num;
+   END FGET_NUM_TEACHERS;
+BEGIN
+   DBMS_OUTPUT.PUT_LINE(FGET_NUM_TEACHERS('ИДиП'));
+END;
 
-begin
-    DBMS_OUTPUT.PUT_LINE(FGET_NUM_TEACHERS('ИДиП'));
-end;
-
-create or replace function C##PVG_P.GET_NUM_SUBJECTS (PCODE SUBJECT.PULPIT%TYPE) return number
-is
-    result_num number;
-begin
-    select count(SUBJECT) into result_num from SUBJECT where PULPIT=PCODE;
-    return result_num;
-end GET_NUM_SUBJECTS;
-
-begin
-    DBMS_OUTPUT.PUT_LINE(GET_NUM_SUBJECTS('ИСиТ'));
-end;
+DECLARE
+   FUNCTION GET_NUM_SUBJECTS(PCODE IN SUBJECT.PULPIT%TYPE) RETURN NUMBER IS 
+      result_num NUMBER;
+   BEGIN
+      SELECT COUNT(SUBJECT) INTO result_num FROM SUBJECT WHERE PULPIT = PCODE;
+      RETURN result_num;
+   END GET_NUM_SUBJECTS;
+BEGIN
+   DBMS_OUTPUT.PUT_LINE(GET_NUM_SUBJECTS('ИСиТ'));
+END;
 ------------------------------------ Задание №5
-create or replace package TEACHERS as
-  FCODE FACULTY.FACULTY%type;
-  PCODE SUBJECT.PULPIT%type;
-  procedure P_GET_TEACHERS(FCODE FACULTY.FACULTY%type);
-  procedure P_GET_SUBJECTS (PCODE SUBJECT.PULPIT%type);
-  function P_GET_NUM_TEACHERS(FCODE FACULTY.FACULTY%type) return number;
-  function P_GET_NUM_SUBJECTS(PCODE SUBJECT.PULPIT%type) return number;
-end TEACHERS;
+CREATE OR REPLACE PACKAGE TEACHERS AS
+  FCODE FACULTY.FACULTY%TYPE;
+  PCODE SUBJECT.PULPIT%TYPE;
+  PROCEDURE P_GET_TEACHERS(FCODE FACULTY.FACULTY%TYPE);
+  PROCEDURE P_GET_SUBJECTS (PCODE SUBJECT.PULPIT%TYPE);
+  FUNCTION P_GET_NUM_TEACHERS(FCODE FACULTY.FACULTY%TYPE) RETURN NUMBER;
+  FUNCTION P_GET_NUM_SUBJECTS(PCODE SUBJECT.PULPIT%TYPE) RETURN NUMBER;
+END TEACHERS;
 
-create or replace package body TEACHERS
-is
-    procedure P_GET_TEACHERS(FCODE FACULTY.FACULTY%TYPE)
-        is
-        cursor GetTeachersByFaculty is
-            select TEACHER, TEACHER_NAME, P.PULPIT
-            from TEACHER
-                     inner join PULPIT P on P.PULPIT = TEACHER.PULPIT
-            where FACULTY = FCODE;
-        m_teacher      TEACHER.TEACHER%TYPE;
-        m_teacher_name TEACHER.TEACHER_NAME%TYPE;
-        m_pulpit       TEACHER.PULPIT%TYPE;
-    begin
-        open GetTeachersByFaculty;
-        fetch GetTeachersByFaculty into m_teacher, m_teacher_name, m_pulpit;
+CREATE OR REPLACE PACKAGE BODY TEACHERS
+IS
+    PROCEDURE P_GET_TEACHERS(FCODE FACULTY.FACULTY%TYPE)
+        IS
+        CURSOR GETTEACHERSBYFACULTY IS
+            SELECT TEACHER, TEACHER_NAME, P.PULPIT
+            FROM TEACHER
+                     INNER JOIN PULPIT P ON P.PULPIT = TEACHER.PULPIT
+            WHERE FACULTY = FCODE;
+        M_TEACHER      TEACHER.TEACHER%TYPE;
+        M_TEACHER_NAME TEACHER.TEACHER_NAME%TYPE;
+        M_PULPIT       TEACHER.PULPIT%TYPE;
+    BEGIN
+        OPEN GETTEACHERSBYFACULTY;
+        FETCH GETTEACHERSBYFACULTY INTO M_TEACHER, M_TEACHER_NAME, M_PULPIT;
 
-        while (GetTeachersByFaculty%found)
-            loop
-                DBMS_OUTPUT.PUT_LINE(m_teacher || ' ' || m_teacher_name || ' ' || m_pulpit);
-                fetch GetTeachersByFaculty into m_teacher, m_teacher_name, m_pulpit;
-            end loop;
+        WHILE (GETTEACHERSBYFACULTY%FOUND)
+            LOOP
+                DBMS_OUTPUT.PUT_LINE(M_TEACHER || ' ' || M_TEACHER_NAME || ' ' || M_PULPIT);
+                FETCH GETTEACHERSBYFACULTY INTO M_TEACHER, M_TEACHER_NAME, M_PULPIT;
+            END LOOP;
 
-        close GetTeachersByFaculty;
+        CLOSE GETTEACHERSBYFACULTY;
 
-    end P_GET_TEACHERS;
-    procedure P_GET_SUBJECTS(PCODE SUBJECT.PULPIT%TYPE)
-        is
-        cursor GetSubjects is
-            select *
-            from SUBJECT
-            where PULPIT = PCODE;
-        m_subject      SUBJECT.SUBJECT%type;
-        m_subject_name SUBJECT.SUBJECT_NAME%type;
-        m_pulpit       SUBJECT.PULPIT%type;
-    begin
-        open GetSubjects;
-        fetch GetSubjects into m_subject, m_subject_name, m_pulpit;
+    END P_GET_TEACHERS;
+    PROCEDURE P_GET_SUBJECTS(PCODE SUBJECT.PULPIT%TYPE)
+        IS
+        CURSOR GETSUBJECTS IS
+            SELECT *
+            FROM SUBJECT
+            WHERE PULPIT = PCODE;
+        M_SUBJECT      SUBJECT.SUBJECT%TYPE;
+        M_SUBJECT_NAME SUBJECT.SUBJECT_NAME%TYPE;
+        M_PULPIT       SUBJECT.PULPIT%TYPE;
+    BEGIN
+        OPEN GETSUBJECTS;
+        FETCH GETSUBJECTS INTO M_SUBJECT, M_SUBJECT_NAME, M_PULPIT;
 
-        while (GetSubjects%found)
-            loop
-                DBMS_OUTPUT.PUT_LINE(m_subject || ' ' || m_subject_name || ' ' || m_pulpit);
-                fetch GetSubjects into m_subject, m_subject_name, m_pulpit;
-            end loop;
-        close GetSubjects;
+        WHILE (GETSUBJECTS%FOUND)
+            LOOP
+                DBMS_OUTPUT.PUT_LINE(M_SUBJECT || ' ' || M_SUBJECT_NAME || ' ' || M_PULPIT);
+                FETCH GETSUBJECTS INTO M_SUBJECT, M_SUBJECT_NAME, M_PULPIT;
+            END LOOP;
+        CLOSE GETSUBJECTS;
 
-    end P_GET_SUBJECTS;
-    function P_GET_NUM_TEACHERS(FCODE FACULTY.FACULTY%TYPE)
-        return number
-        is
-        result_num number;
-    begin
-        select count(TEACHER)
-        into result_num
-        from TEACHER
-        where TEACHER.PULPIT in (select PULPIT from PULPIT where FACULTY = FCODE);
-        return result_num;
-    end P_GET_NUM_TEACHERS;
-    function P_GET_NUM_SUBJECTS(PCODE SUBJECT.PULPIT%TYPE) return number
-        is
-        result_num number;
-    begin
-        select count(SUBJECT) into result_num from SUBJECT where PULPIT = PCODE;
-        return result_num;
-    end P_GET_NUM_SUBJECTS;
-begin
-    null;
-end TEACHERS;
+    END P_GET_SUBJECTS;
+    FUNCTION P_GET_NUM_TEACHERS(FCODE FACULTY.FACULTY%TYPE)
+        RETURN NUMBER
+        IS
+        RESULT_NUM NUMBER;
+    BEGIN
+        SELECT COUNT(TEACHER)
+        INTO RESULT_NUM
+        FROM TEACHER
+        WHERE TEACHER.PULPIT IN (SELECT PULPIT FROM PULPIT WHERE FACULTY = FCODE);
+        RETURN RESULT_NUM;
+    END P_GET_NUM_TEACHERS;
+    FUNCTION P_GET_NUM_SUBJECTS(PCODE SUBJECT.PULPIT%TYPE) RETURN NUMBER
+        IS
+        RESULT_NUM NUMBER;
+    BEGIN
+        SELECT COUNT(SUBJECT) INTO RESULT_NUM FROM SUBJECT WHERE PULPIT = PCODE;
+        RETURN RESULT_NUM;
+    END P_GET_NUM_SUBJECTS;
+BEGIN
+    NULL;
+END TEACHERS;
 
 ------------------------------------ Задание №6
 begin
